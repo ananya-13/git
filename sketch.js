93a29f1cbb631d6275dd0750390520d7c6df5365
ananya-13 <ananya13092006@gmail.com>
2021-05-10 19:47:33 +0530

    new ground

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
+    ground1=new  Ground(300,200,60,20);
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
@@ -47,6 +48,7 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
+    ground1.display();
     pig1.display();
     log1.display();
 
